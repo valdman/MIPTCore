f0909a1001c49e11577e8981c86a9fdedf7d91c4
Boris Valdman <boris.valdman@live.ru>
2019-02-13 20:47:51 +0300

    best2pay callback fix

--- MIPTCore/Models/PaymentCallbackModel.cs
+++ MIPTCore/Models/PaymentCallbackModel.cs
@@ -1,54 +1,22 @@
-﻿using System.Xml.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MIPTCore.Models
 {
-    [XmlRoot("operation")]
     public class PaymentCallbackModel
     {
-        [XmlElement("order_id")]
-        public string OrderId { get; set; }   
+        [Required]
+        public string MdOrder { get; set; }
         
-        [XmlElement("order_state")]
-        public string OrderState { get; set; }
+        [Required]
+        public int OrderNumber { get; set; }
         
-        [XmlElement("reference")]
-        public int Reference { get; set; }
+        public string Checksum { get; set; }
         
-        [XmlElement("id")]
-        public int Id { get; set; }
-        
-        [XmlElement("date")]
-        public string Date { get; set; }
-        
-        [XmlElement("type")]
-        public string Type { get; set; }
-        
-        [XmlElement("state")]
-        public string State { get; set; }
-        
-        [XmlElement("reason_code")]
-        public string ReasonCode { get; set; }
-        
-        [XmlElement("name")]
-        public string Name { get; set; }
-        
-        [XmlElement("pan")]
-        public string Pan { get; set; }
-        
-        [XmlElement("email")]
-        public string Email { get; set; }
-        
-        [XmlElement("amount")]
-        public string Amount { get; set; }
-        
-        [XmlElement("currency")]
-        public string Currency { get; set; }
-        
-        [XmlElement("approval_code")]
-        public string ApprovalCode { get; set; }
-        
-        [XmlElement("signature")]
-        public string Signature { get; set; }
+        [Required]
+        public string Operation { get; set; }
+
+        [Required]
+        public PaymentOperationStatus Status { get; set; }
     }
 
     public enum PaymentOperationStatus
